1ac5c733bd71207a5c87ffb78edc67ef1b12bd0e
DavVarr <davide.varricchio2@gmail.com>
2024-06-02 17:52:57 +0200

    added serialization annotation

--- src/main/java/player/AbstractPlayer.java
+++ src/main/java/player/AbstractPlayer.java
@@ -1,6 +1,9 @@
 package player;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public abstract class AbstractPlayer {
 
     protected int id;
