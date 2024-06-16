4a0ef9dece67a759ad93f3f6d76424894b3e1ecb
DavVarr <davide.varricchio2@gmail.com>
2024-06-16 02:50:15 +0200

    Added enum for phase representation

---
+++ src/main/java/player/Phase.java
@@ -0,0 +1,12 @@
+package player;
+
+/**
+ * Enum that represents the possible phases of the WatchOut game
+ */
+public enum Phase {
+    UNKNOWN,
+    PREPARATION,
+    ELECTION,
+    GAME,
+    END;
+}
